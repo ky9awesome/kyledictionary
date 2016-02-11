d68a46851c6600575d343ba5097edc56b8f1a173
Kyle Knoche <kyle.knoche@gmail.com>
2016-02-11 12:32:30 -0500

    Create database

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from app import db
+
+class Definition(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    author = db.Column(db.String(20))
+    word = db.Column(db.String(64), index=True)
+    meaning = db.Column(db.String(1000))
+    views = db.Column(db.Integer)
+    votes_for = db.Column(db.Integer)
+    votes_against = db.Column(db.Integer)
+
+    def __repr__(self):
+        return '<Definition %r>' % (self.word  + " : " + self.meaning)
