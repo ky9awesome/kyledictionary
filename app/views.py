1627e1e17b8584dfd92469206560a6b16adc5e02
Kyle Knoche <kyle.knoche@gmail.com>
2016-02-11 12:12:47 -0500

    Render template with date

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from flask import render_template
 from app import app
+import datetime
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world"
+    today = datetime.datetime.utcnow().strftime("%B %d, %Y")   # eg, February 11, 2016
+    return render_template('index.html',
+                           today = today)
