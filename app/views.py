0c5275f718d4a5d0269bfcff1d751f59ee58da09
Kyle Knoche <kyle.knoche@gmail.com>
2016-02-13 13:27:04 -0500

    Updated index view dates/times

--- app/views.py
+++ app/views.py
@@ -2,9 +2,21 @@ from flask import render_template
 from app import app
 import datetime
 
+
+# helper functions
+def get_date():
+    return datetime.datetime.utcnow().strftime("%B %d, %Y") # eg, February 11, 2016
+
+def get_time():
+    return datetime.datetime.utcnow().strftime("%I:%M:%S%p") # eg, 2:23:46PM
+
+
+# views
 @app.route('/')
 @app.route('/index')
 def index():
-    today = datetime.datetime.utcnow().strftime("%B %d, %Y")   # eg, February 11, 2016
-    return render_template('index.html',
-                           today = today)
+    today = get_date()
+    time = get_time()
+    return render_template('_base.html',
+                           today=today,
+                           time=time)
