eb49f25f9642aea5cab85cb82087bde024a46505
Kyle Knoche <kyle.knoche@gmail.com>
2016-02-14 17:56:32 -0500

    Add database

--- app/views.py
+++ app/views.py
@@ -90,6 +90,12 @@ def search_result(q, page=1):
     today = get_date()
     time = get_time
     results = models.Definition.query.filter_by(word=q).order_by(models.Definition.votes_for.desc()).paginate(page, POSTS_PER_PAGE, False)
+    if results.items == []:
+        return render_template('search.html',
+                               today=today,
+                               time=time,
+                               callout=True,   # instead of showing results, invite user to add definition
+                               query=q)
     return render_template('search.html',
                            today=today,
                            time=time,
